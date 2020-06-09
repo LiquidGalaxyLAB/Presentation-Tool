b037a121c95ac7afd07ef093abaab06a025c6d4a
Karine Pistili <karine.pistili@gmail.com>
2020-06-08 22:33:48 -0300

    basic information form template

--- frontend/presentation-tool/src/components/molecules/BasicInfoForm.vue
+++ frontend/presentation-tool/src/components/molecules/BasicInfoForm.vue
@@ -1,73 +1,134 @@
 <template>
   <v-container>
+    <div class="pb-12">
+      <h1>Fill up the basic information</h1>
+      <v-divider></v-divider>
+    </div>
     <v-form ref="form" v-model="valid" lazy-validation>
-      <v-text-field v-model="name" :counter="20" :rules="nameRules" label="Title" filled required></v-text-field>
-
-      <v-textarea v-model="description" label="Description" filled required></v-textarea>
-
-    <v-row>
-        <v-col>
-            <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Category is required']"
-        label="Category"
-        filled
+      <v-text-field
+        v-model="presentation.title"
+        :counter="20"
+        :rules="titleRules"
+        label="Title *"
+        outlined
         required
-      ></v-select>
-        </v-col>
-        <v-col>
-            <v-text-field v-model="name" :counter="20" :rules="nameRules" label="Audio?" filled required></v-text-field>
-        </v-col>
-    </v-row>
-      
-
+        hint="Choose an intuitive title with less than 20 characters"
+        persistent-hint
+      ></v-text-field>
+      <v-textarea
+        v-model="presentation.description"
+        label="Description"
+        outlined
+        required
+        hint="Describe what your presentation will be about"
+        persistent-hint
+      ></v-textarea>
       <v-row>
-        <v-col>
+        <v-col cols="12" md="6">
           <v-select
-            v-model="select"
-            :items="items"
+            v-model="presentation.category"
+            :items="categories"
             :rules="[v => !!v || 'Category is required']"
-            label="Screens"
-            filled
+            label="Category *"
+            outlined
             required
+            hint="The category which your presentation best fits in"
+            persistent-hint
           ></v-select>
         </v-col>
-        <v-col>
+        <v-col cols="12" md="6">
+          <!--weird bug with value-->
+          <v-file-input
+            v-model="presentation.audio"
+            clearable
+            outlined
+            show-size
+            label="Audio"
+            hint="NOTE: this audio will play through the whole presentation. If you don't want that, don't upload here"
+            persistent-hint
+          ></v-file-input>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6">
           <v-select
-            v-model="select"
-            :items="items"
-            :rules="[v => !!v || 'Category is required']"
-            label="Slides"
-            filled
+            v-model="presentation.screensqt"
+            :items="screens"
+            :rules="[v => !!v || 'Number of screens is required']"
+            label="Screens *"
+            outlined
             required
+            hint="The number of screens in your Liquid Galaxy"
+            persistent-hint
+          ></v-select>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-select
+            v-model="presentation.slidesqt"
+            :items="slides"
+            :rules="[v => !!v || 'Number of slides is required']"
+            label="Slides *"
+            outlined
+            required
+            hint="The quatity of slides you are going to need"
+            persistent-hint
           ></v-select>
         </v-col>
       </v-row>
-
-      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Save</v-btn>
+      <v-row justify="space-between" class="pl-2">
+        <v-btn color="red" text class="mr-4" @click="$router.push('/')">Cancel</v-btn>
+        <v-btn  color="blue" class="mr-4" dark @click="validate">Save and continue</v-btn>
+      </v-row>
     </v-form>
   </v-container>
 </template>
 
 <script>
 export default {
+   props: {
+    value: String
+  },
+
   data: () => ({
-    valid: true,
-    name: "",
-    nameRules: [
-      v => !!v || "Name is required",
+    //models
+    valid: true, 
+    presentation: {
+      title: "",
+      description:"",
+      category: "",
+      audio:null,
+      screensqt:"",
+      slidesqt:""
+    },
+    //rules
+    titleRules: [
+      v => !!v || "Title is required",
       v => (v && v.length <= 20) || "Name must be less than 20 characters"
     ],
-    select: null,
-    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
-    checkbox: false
+    //selection data
+    categories: ["Education", "Travel", "Nature", "Real State", "History"],
+    screens: ["1", "2", "3", "4", "5", "6","7"],
+    slides: ["1", "2", "3", "4", "5"],
   }),
 
   methods: {
     validate() {
-      this.$refs.form.validate();
+      if(this.$refs.form.validate()){
+        this.step='2'
+        //other validations
+      }
+      
     }
-  }
+  },
+computed: {
+    step: {
+     get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      }
+    },
+  },
 };
 </script>
--- frontend/presentation-tool/src/components/organisms/ProgressStepper.vue
+++ frontend/presentation-tool/src/components/organisms/ProgressStepper.vue
@@ -1,76 +1,42 @@
 <template>
-<v-container>
-  <v-stepper v-model="e1">
-    <v-stepper-header>
-      <v-stepper-step editable :complete="e1 > 1" step="1" color="pink">Basic Information</v-stepper-step>
-
-      <v-divider></v-divider>
-
-      <v-stepper-step editable :complete="e1 > 2" step="2" color="pink">Build Slides</v-stepper-step>
-
-      <v-divider></v-divider>
-
-      <v-stepper-step editable :complete="e1 > 3" step="3" color="pink">Finish</v-stepper-step>
-
-    </v-stepper-header>
-
-    <v-stepper-items>
-      <v-stepper-content step="1">
-        <basic-info-form></basic-info-form>
-
-        <v-btn
-          color="primary"
-          @click="e1 = 2"
-        >
-          Continue
-        </v-btn>
-
-        <v-btn text>Cancel</v-btn>
-      </v-stepper-content>
-
-      <v-stepper-content step="2">
-       <slide-builder></slide-builder>
-
-        <v-btn
-          color="primary"
-          @click="e1 = 3"
-        >
-          Continue
-        </v-btn>
-
-        <v-btn text>Cancel</v-btn>
-      </v-stepper-content>
-
-       <v-stepper-content step="3">
-       <div>finish</div>
-
-        <v-btn
-          color="primary"
-          @click="e1 = 3"
-        >
-          Continue
-        </v-btn>
-
-        <v-btn text>Cancel</v-btn>
-      </v-stepper-content>
-    </v-stepper-items>
-  </v-stepper>
-</v-container>
+  <v-container>
+    <v-stepper v-model="e1">
+      <v-stepper-header>
+        <v-stepper-step editable :complete="e1 > 1" step="1" color="pink">Basic Information</v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step editable :complete="e1 > 2" step="2" color="pink">Build Slides</v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step editable :complete="e1 > 3" step="3" color="pink">Finish</v-stepper-step>
+      </v-stepper-header>
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <basic-info-form v-model="e1"></basic-info-form>
+        </v-stepper-content>
+        <v-stepper-content step="2">
+          <slide-builder></slide-builder>
+        </v-stepper-content>
+        <v-stepper-content step="3">
+          <div>finish</div>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
+  </v-container>
 </template>
 
 <script>
-import BasicInfoForm from '@/components/molecules/BasicInfoForm.vue'
-import SlideBuilder from "@/components/molecules/SlideBuilder.vue"
+import BasicInfoForm from "@/components/molecules/BasicInfoForm.vue";
+import SlideBuilder from "@/components/molecules/SlideBuilder.vue";
 
-  export default {
-    components:{
-        BasicInfoForm,
-        SlideBuilder
-    },
-    data () {
-      return {
-        e1: 1,
-      }
-    },
+export default {
+  components: {
+    BasicInfoForm,
+    SlideBuilder
+  },
+  data() {
+    return {
+      e1: "1"
+    };
   }
+
+};
 </script>
