3180de5ceb08afa3af0d12932ef9ea763f97b58d
Karine Pistili <karine.pistili@gmail.com>
2020-07-13 18:42:54 -0300

    added overlay and enable/disable editing on basic form

--- frontend/presentation-tool/src/components/molecules/BasicInfoForm.vue
+++ frontend/presentation-tool/src/components/molecules/BasicInfoForm.vue
@@ -5,6 +5,11 @@
       <v-divider></v-divider>
     </div>
     <v-form ref="form" v-model="valid" lazy-validation>
+      <v-overlay :value="edit" :absolute="true" >
+        <v-btn dark @click="edit = false">edit
+          <v-icon right>mdi-pencil</v-icon>
+        </v-btn>
+      </v-overlay>
       <v-text-field
         v-model="presentation.title"
         :counter="20"
@@ -14,6 +19,7 @@
         required
         hint="Choose an intuitive title with less than 20 characters"
         persistent-hint
+        :readonly="edit"
       ></v-text-field>
       <v-textarea
         v-model="presentation.description"
@@ -22,6 +28,7 @@
         required
         hint="Describe what your presentation will be about"
         persistent-hint
+        :readonly="edit"
       ></v-textarea>
       <v-row>
         <v-col cols="12" md="6">
@@ -34,6 +41,7 @@
             required
             hint="The category which your presentation best fits in"
             persistent-hint
+            :readonly="edit"
           ></v-select>
         </v-col>
         <v-col cols="12" md="6">
@@ -46,10 +54,11 @@
             required
             hint="The number of screens in your Liquid Galaxy"
             persistent-hint
+            :readonly="edit"
           ></v-text-field>
         </v-col>
       </v-row>
-      <v-switch v-model="audio" label="One audio for the whole presentation"></v-switch>
+      <v-switch v-model="audio" label="One audio for the whole presentation" :readonly="edit"></v-switch>
       <v-file-input
         v-if="audio"
         v-model="presentation.file"
@@ -59,8 +68,9 @@
         label="Audio"
         hint="NOTE: this audio will play through the whole presentation. If you don't want that, don't upload here"
         persistent-hint
+        :readonly="edit"
       ></v-file-input>
-      <v-row justify="center" class="pl-2 pt-5 pb-4">
+      <v-row justify="center" class="pl-2 pt-5 pb-4" v-if="!edit">
         <v-btn color="blue" class="mr-4" dark @click="validate()">Save</v-btn>
       </v-row>
     </v-form>
@@ -73,6 +83,7 @@ export default {
     //models
     valid: true,
     audio: false,
+    edit:false,
     presentation: {
       title: "",
       maxscreens:"",
@@ -98,6 +109,7 @@ export default {
     validate() {
       if (this.$refs.form.validate()) {
         this.$store.dispatch('presentationBasicInformation',this.presentation)
+        this.edit = true
       }
     },
    
--- frontend/presentation-tool/src/views/Builder.vue
+++ frontend/presentation-tool/src/views/Builder.vue
@@ -25,12 +25,12 @@
       </v-row>
 
       <v-row>
-        <v-col cols="12" md="4" sm="6" xs="12">
+        <v-col cols="12" md="4" sm="4" xs="12">
           <v-card width="100%">
             <basic-info-form></basic-info-form>
           </v-card>
         </v-col>
-        <v-col cols="12" md="8" sm="6" xs="12">
+        <v-col cols="12" md="8" sm="8" xs="12">
           <v-card width="100%" height="100%" min-height="500">
             <slide-builder></slide-builder>
           </v-card>
