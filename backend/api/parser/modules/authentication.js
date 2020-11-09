5d3e1a7e7b62cc83b79d4cabb141f488dcf97ff8
Karine <karine.pistili@gmail.com>
2020-11-09 15:02:13 -0300

    added refresh token

--- backend/api/parser/modules/authentication.js
+++ backend/api/parser/modules/authentication.js
@@ -1,16 +1,24 @@
 var jwt = require('jsonwebtoken')
 
 module.exports = {
-    authenticateToken: function (req,res,next){
+    authenticateToken: function (req, res, next) {
         const authHeader = req.headers['authorization']
         const token = authHeader && authHeader.split(' ')[1]
 
-        if(token == null || token == undefined) return res.status(401).json('No Token')
+        if (token == null || token == undefined) return res.status(401).json('No Token')
 
-        jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, userId) =>{
-            if(err) return res.status(403).json('Forbidden')
+        jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, userId) => {
+            if (err) return res.status(403).json('Forbidden')
             req.userId = userId
             next()
         })
+    },
+    generateAccessToken: function (id) {
+        return jwt.sign({ userId: id }, process.env.SECRET_ACCESS_TOKEN, {
+            expiresIn: 60 // expires in 1min
+        });
+    },
+    generateRefreshToken: function (id){
+        return jwt.sign({ userId: id }, process.env.SECRET_REFRESH_TOKEN);
     }
 }
--- backend/api/routes/authentication.js
+++ backend/api/routes/authentication.js
@@ -1,23 +1,41 @@
 const express = require('express')
 const router = express.Router()
-var jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
+const { generateAccessToken } = require('../parser/modules/authentication')
+const auth = require('../parser/modules/authentication')
+
+//This is not a production way of doing things. The correct way is to store the refreshTokens inside a regular db or caching with redis
+var refreshTokens = [] 
+
+router.post('/token',(req,res) =>{
+  const refreshToken = req.body.token
+  if(refreshToken == null) return res.sendStatus(401)
+  if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.SECRET_REFRESH_TOKEN, (err, userId) => {
+    if(err) return res.sendStatus(403)
+    const accessToken = generateAccessToken(userId)
+    res.json({accessToken: accessToken})
+  })
+})
 
 router.post('/login',(req,res,next) =>{
-    if(req.body.user === 'luiz' && req.body.pwd === '123'){
-        
+    if(req.body.user === 'luiz' && req.body.pwd === '123'){  
         //MOCK INFO ABOUT USER
         const id = 1; 
-        var token = jwt.sign({userId: id}, process.env.SECRET_ACCESS_TOKEN, {
-          expiresIn: 60 // expires in 5min
-        });
-        return res.json({ auth: true, token: token });
+        const accessToken = auth.generateAccessToken(id)
+        const refreshToken = auth.generateRefreshToken(id)
+       
+        //only for example, this needs to be saved in a db or cache service
+        refreshTokens.push(refreshToken)
+        return res.json({ accessToken: accessToken, refreshToken: refreshToken });
       }
       
       res.status(500).json({message: 'Login inválido!'});
 })
 
-router.post('/logout', function(req, res) {
-    res.json({ auth: false, token: null });
+router.delete('/logout', function(req, res) {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
 })
 
 module.exports = router
